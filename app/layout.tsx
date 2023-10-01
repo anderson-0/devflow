a23ecf81d12c35c3235c1c81535c085e5ad1367d
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-01 19:56:23 -0300

    feat: include fonts into global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,19 @@
+import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
   title: "DevFlow",
@@ -20,7 +31,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className} ${spaceGrotesk.className}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
