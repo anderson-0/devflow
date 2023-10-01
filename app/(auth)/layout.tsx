232df00e192cab25e74a4d2dbcecc0c451efd853
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-01 18:33:08 -0300

    feat: centered auth layout and created root home page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      {children}
+    </main>
+  );
 };
 
 export default Layout;
