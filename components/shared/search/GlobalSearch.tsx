cccfa201eeb58c6e78cf6374b1358ae55fbe1ac6
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-02 14:23:17 -0300

    feat: GlobalSearch with position and icon

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const GlobalSearch = () => {
-  return <div>GlobalSearch</div>;
+  return (
+    <div className="relative w-full max-w-[600px] max-lg:hidden">
+      <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+        <Image
+          src="/assets/icons/search.svg"
+          width={20}
+          height={20}
+          alt="search"
+          className=" cursor-pointer"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default GlobalSearch;
