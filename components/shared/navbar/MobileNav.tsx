b80b517d07b6fdd4bdc4d2d07f9dcc7512100a73
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-02 13:48:22 -0300

    feat: mobile navbar list links with buttons

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
@@ -5,13 +6,47 @@ import {
   SheetContent,
   SheetClose,
 } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
 import { SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const NavContent = () => {
-  return <p>hello</p>;
+  const pathname = usePathname();
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((item) => {
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+        return (
+          <SheetClose asChild key={item.route}>
+            <Link
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light-900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
 };
 
 const MobileNav = () => {
@@ -42,7 +77,7 @@ const MobileNav = () => {
           </p>
         </Link>
         <div>
-          <SheetClose className="absolute right-0 top-0 mr-5 mt-5">
+          <SheetClose>
             <NavContent />
           </SheetClose>
 
