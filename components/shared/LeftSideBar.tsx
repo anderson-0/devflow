6ccf445dcb90faf4998ca83e11e72d61c051bc1e
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-04 22:58:08 -0300

    feat: leftsidebar hidding on small screens

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -1,7 +1,62 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "../ui/button";
 
 const LeftSideBar = () => {
-  return <div>LeftSideBar</div>;
+  const pathname = usePathname();
+  return (
+    <section className="background-light900_dark200 light-border boder-r sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+      <div className="flex grow flex-col gap-6">
+        {sidebarLinks.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
+          return (
+            <Link
+              key={item.route}
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+      <SignedOut>
+        <div className="flex flex-col gap-3">
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3">
+              <span className="primary-text-gradient">Log In</span>
+            </Button>
+          </Link>
+
+          <Link href="/sign-up">
+            <Button className="small-medium btn-tertiary light-border-2 text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3">
+              Sign Up
+            </Button>
+          </Link>
+        </div>
+      </SignedOut>
+    </section>
+  );
 };
 
 export default LeftSideBar;
