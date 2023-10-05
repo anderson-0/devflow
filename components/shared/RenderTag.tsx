10d039f1399772d9be27fae8ef16913607a40916
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-05 12:29:16 -0300

    feat: badge styling

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { Badge } from "../ui/badge";
 
 interface RenderTagProps {
   _id: number;
@@ -16,7 +17,14 @@ const RenderTag = ({
 }: RenderTagProps) => {
   return (
     <Link href={`/tags/${_id}`} className="flex justify-between gap-2">
-      {name}
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+        {name}
+      </Badge>
+      {showCount && (
+        <span className="small-medium text-dark500_light700">
+          {totalQuestions}
+        </span>
+      )}
     </Link>
   );
 };
