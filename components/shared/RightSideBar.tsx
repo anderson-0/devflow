9f884ef06ec9058876c1af18412424dd7736e6e2
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-07 14:07:16 -0300

    fix: updated hardcoded topquestions _id property to string

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -6,24 +6,24 @@ import RenderTag from "./RenderTag";
 const RightSideBar = () => {
   const topQuestions = [
     {
-      _id: 1,
+      _id: "1",
       title:
         "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
     },
     {
-      _id: 2,
+      _id: "2",
       title: "How do I use express as a custom server in NextJS?",
     },
     {
-      _id: 3,
+      _id: "3",
       title: "Can I get the course for free?",
     },
     {
-      _id: 4,
+      _id: "4",
       title: "Redux Toolkit Not Updating State as Expected",
     },
     {
-      _id: 5,
+      _id: "5",
       title: "Async/Await Function Not Handling Errors Properly",
     },
   ];
