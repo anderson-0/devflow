87a98e7e495cc824ec285433e20fb7a36bdef2a3
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-07 14:08:36 -0300

    fix: fixed popularTags hardcoded array _id property

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -29,27 +29,27 @@ const RightSideBar = () => {
   ];
   const popularTags = [
     {
-      _id: 1,
+      _id: "1",
       name: "javascript",
       totalQuestions: 5,
     },
     {
-      _id: 2,
+      _id: "2",
       name: "react",
       totalQuestions: 3,
     },
     {
-      _id: 3,
+      _id: "3",
       name: "next",
       totalQuestions: 10,
     },
     {
-      _id: 4,
+      _id: "4",
       name: "vue",
       totalQuestions: 1,
     },
     {
-      _id: 5,
+      _id: "5",
       name: "redux",
       totalQuestions: 6,
     },
