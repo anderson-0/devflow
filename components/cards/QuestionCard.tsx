026a73d53e8da733a820890ec08dc3bb96bf5daa
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-08 19:28:33 -0300

    feat: questioncard component with timestamp and metrics

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import React from "react";
 import RenderTag from "../shared/RenderTag";
+import Metric from "../shared/Metric";
+import { getTimestamp } from "@/lib/utils";
 
 interface QuestionCardProps {
   _id: number;
@@ -28,7 +30,7 @@ const QuestionCard = ({
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
-            {String(createdAt)}
+            {getTimestamp(createdAt)}
           </span>
           <Link href={`/question/${_id}`}>
             <h3 className="sm:h3-semibold base-semibold text-dark_light900 line-clamp-1 flex-1">
@@ -42,6 +44,38 @@ const QuestionCard = ({
         {tags.map((tag) => (
           <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
         ))}
+      </div>
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          imgUrl="/assets/icons/avatar.svg"
+          alt="user"
+          value={author.name}
+          title={`asked ${getTimestamp(createdAt)}`}
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="Upvotes"
+          value={upvotes}
+          title="Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={answers.length}
+          title="Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={views}
+          title="Views"
+          textStyles="small-medium text-dark400_light800"
+        />
       </div>
     </div>
   );
