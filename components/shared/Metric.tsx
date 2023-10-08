0cea4a6951f261ad596921edf68a13ee204164e7
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-08 13:17:15 -0300

    feat: metric component

---
+++ components/shared/Metric.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+
+interface MetricProps {
+  imgUrl: string;
+  alt: string;
+  value: string | number;
+  title: string;
+  textStyles?: string;
+  href?: string;
+  isAuthor?: boolean;
+}
+
+const Metric = ({
+  imgUrl,
+  alt,
+  value,
+  title,
+  textStyles,
+  href,
+  isAuthor,
+}: MetricProps) => {
+  return (
+    <div className="flex-center flex-wrap gap-1">
+      <Image
+        src={imgUrl}
+        width={16}
+        height={16}
+        alt={alt}
+        className={`object-contain ${href ? "rounded-full" : ""}`}
+      />
+      <p className={`${textStyles} flex items-center gap-1`}>
+        {value} {title}
+      </p>
+    </div>
+  );
+};
+
+export default Metric;
