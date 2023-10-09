a9fe9071863417722e7e72cde988e9ae65ae819c
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-09 20:46:48 -0300

    fix: using fake clerkId and sending object as params

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -5,9 +5,10 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const AskQuestion = async () => {
-  const { userId } = auth();
+  // const { userId } = auth();
+  const userId = "clerk12345"; // fake user id
   if (!userId) redirect("/sign-in");
-  const mongoUser = await getUserById(userId);
+  const mongoUser = await getUserById({ userId });
   console.log(mongoUser);
 
   return (
