e670b82120a8fd7c7290dffbc84724365b9cf795
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-09 20:46:23 -0300

    refactor: added try catch validation to getUserById server action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -4,8 +4,13 @@ import User from '@/database/user.model'
 import { connectToDatabase } from '../mongoose'
 
 export async function getUserById(params: any) {
-  const { userId } = params;
-  connectToDatabase()
-  const user = await User.findOne({ clerkId: userId });
-  return user;
+  try {
+    const { userId } = params;
+    connectToDatabase()
+    const user = await User.findOne({ clerkId: userId });
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
