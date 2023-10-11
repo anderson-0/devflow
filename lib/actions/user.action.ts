4c9e488b931b83315b657ef23d2db27d2b2952af
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-11 16:46:55 -0300

    feat: crud user server actions

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,9 @@
 
 import User from '@/database/user.model'
 import { connectToDatabase } from '../mongoose'
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from './shared.types';
+import { revalidatePath } from 'next/cache';
+import Question from '@/database/question.model';
 
 export async function getUserById(params: any) {
   try {
@@ -9,6 +12,48 @@ export async function getUserById(params: any) {
     await connectToDatabase()
     const user = await User.findOne({ clerkId: userId });
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    await connectToDatabase()
+    const user = await User.create(userData);
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    await connectToDatabase()
+    const { clerkId, updateData, path } = params;
+    const user = await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+    revalidatePath(path)
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    await connectToDatabase()
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+
+    const userQuestionIds = await Question.find({ author: user._id }).distinct('_id');
+    await Question.deleteMany({ author: user._id });
+
+    // Delete questions, answers, comments, etc. associated with the user
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deletedUser;
   } catch (error) {
     console.log(error);
     throw error;
