fd06cd9c2155e0e905ce1b076c9ee72bd871fb0f
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-11 21:39:49 -0300

    feat: display author's picture and fix alignment

--- components/shared/Metric.tsx
+++ components/shared/Metric.tsx
@@ -7,8 +7,8 @@ interface MetricProps {
   alt: string;
   value: string | number;
   title: string;
-  textStyles?: string;
   href?: string;
+  textStyles?: string;
   isAuthor?: boolean;
 }
 
@@ -17,8 +17,8 @@ const Metric = ({
   alt,
   value,
   title,
-  textStyles,
   href,
+  textStyles,
   isAuthor,
 }: MetricProps) => {
   const metricContent = (
@@ -30,8 +30,10 @@ const Metric = ({
         alt={alt}
         className={`object-contain ${href ? "rounded-full" : ""}`}
       />
+
       <p className={`${textStyles} flex items-center gap-1`}>
         {value}
+
         <span
           className={`small-regular line-clamp-1 ${
             isAuthor ? "max-sm:hidden" : ""
@@ -44,7 +46,11 @@ const Metric = ({
   );
 
   if (href) {
-    return <Link href={href}>{metricContent}</Link>;
+    return (
+      <Link href={href} className="flex-center  gap-1">
+        {metricContent}
+      </Link>
+    );
   }
 
   return <div className="flex-center flex-wrap gap-1">{metricContent}</div>;
