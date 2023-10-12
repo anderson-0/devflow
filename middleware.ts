71356c5aec76fe439999d717d68990c8279ad063
Anderson Silva <anderson.consultant.it@outlook.com>
2023-10-11 21:35:26 -0300

    fix: fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
-
+ 
 export default authMiddleware({
   publicRoutes: [
     '/',
     '/api/webhook',
-    '/question/:id',
+    'question/:id',
     '/tags',
     '/tags/:id',
     '/profile/:id',
     '/community',
     '/jobs'
   ],
-  ignoredRoutes: ['/api/chatgpt', '/((?!api|trpc))(_next|.+\..+)(.*)'
-  ],
-  debug: true,
+  ignoredRoutes: [
+    '/api/webhook', '/api/chatgpt'
+  ]
 });
-
+ 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+ 
